6db57cd634e48a5c02be6438f5d1c629e5a244b1
Reina Sherry <reinasherry9@gmail.com>
2025-09-22 18:14:28 +0300

    Update Pattenrns.ino

--- Pattenrns.ino
+++ Pattenrns.ino
@@ -1,5 +1,5 @@
-const int ledPins[4] = {2, 3, 4};
-const int btnPins[4] = {5, 6, 7};
+const int ledPins[3] = {2, 3, 4};
+const int btnPins[3] = {5, 6, 7};
 const int buzzerPin = 9;
   int tones[3] = {262, 330, 392};  
 int sequence[50];   // store up to 50 steps
@@ -7,7 +7,7 @@ int level = 0;
 
 void setup() {
   Serial.begin(9600);
-  for (int i=0; i<4; i++) {
+  for (int i=0; i<3; i++) {
     pinMode(ledPins[i], OUTPUT);
     pinMode(btnPins[i], INPUT_PULLUP);
     pinMode(buzzerPin, OUTPUT);
@@ -37,7 +37,7 @@ void loop() {
 
 
 void newRound() {
-  sequence[level] = random(0, 4);
+  sequence[level] = random(0, 3);
   level++;
   Serial.print("Level: ");
   Serial.println(level);
@@ -67,7 +67,7 @@ bool getPlayerInput() {
 
 int waitForButton() {
   while (true) {
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 3; i++) {
       if (digitalRead(btnPins[i]) == LOW) {
         digitalWrite(ledPins[i], HIGH);
         tone(buzzerPin, tones[i], 300); // beep for button press
